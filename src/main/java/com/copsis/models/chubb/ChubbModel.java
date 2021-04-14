1dc826bae1abddbdc7fb21c603edaa978be3c971
AAC <desarrollo8@copsis.com>
2021-04-14 11:53:30 -0500

    feature/QC-7727-migracion-lector-pdf-a-spring
    Actualizacion

--- src/main/java/com/copsis/models/chubb/ChubbModel.java
+++ src/main/java/com/copsis/models/chubb/ChubbModel.java
@@ -6,6 +6,7 @@ import org.apache.pdfbox.text.PDFTextStripper;
 import com.copsis.models.DataToolsModel;
 import com.copsis.models.EstructuraJsonModel;
 import com.copsis.models.chubb.autos.ChubbAutosModel;
+import com.copsis.models.mapfre.autos.MapfreAutosModel;
 
 import lombok.Data;
 
@@ -27,20 +28,20 @@ public class ChubbModel {
 			for (String tipo : tipos) {
 				if (contenido.contains(tipo)) {
 					switch (tipo) {
-//		                        case "HOGAR":
-//		                        case "SEGURO CONCRETA":
-//		                            pagFin = pagFinRango(stripper, doc, "Notas del riesgo");
-//		                     
-//		                            if (pagFin == 0) {
-//		                                pagFin = pagFinRango(stripper, doc, "FACTURA");
-//		                            }                            
-//		                         
-//		                            if (pagFin > 0) {
-//		                                contenido = "";
-//		                                DatosChubbHogar datosChubbHogar = new DatosChubbHogar(identifica.caratula(1, pagFin, stripper, doc), identifica.recibos(stripper, doc, "AVISO DE COBRO"));
-//		                                jsonObject = datosChubbHogar.procesar();
-//		                            }
-//		                            break;
+		                        case "HOGAR":
+		                        case "SEGURO CONCRETA":
+		                            pagFin = fn.pagFinRango(pdfStripper, pdDoc, "Notas del riesgo");
+		                     
+		                            if (pagFin == 0) {
+		                                pagFin = fn.pagFinRango(pdfStripper, pdDoc, "FACTURA");
+		                            }                            
+		                            if (pagFin > 0) {
+		                                contenido = "";
+		                         
+		                             	modelo = new ChubbDiversosModel(fn.caratula(1, pagFin, pdfStripper, pdDoc),fn.textoBusqueda(pdfStripper, pdDoc, "AVISO DE COBRO",false)).procesar();
+		                        	
+		                            }
+		                            break;
 					case "AUTOMÓVILES":
 					case "TRANSPORTE":
 						pagIni = fn.pagFinRango(pdfStripper, pdDoc, "CARÁTULA");
