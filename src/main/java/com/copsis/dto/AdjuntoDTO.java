1230442fec5f60bdaa9575a097b414acf6061278
AAC <desarrollo8@copsis.com>
2021-06-07 14:17:04 -0500

    bugfix/QC-8878-union-de-pdfs-spring
    se subio cambios

--- src/main/java/com/copsis/dto/AdjuntoDTO.java
+++ src/main/java/com/copsis/dto/AdjuntoDTO.java
@@ -1,8 +1,11 @@
 package com.copsis.dto;
+import lombok.Data;
 
+@Data
 public class AdjuntoDTO {
-	private Integer siniestroID;
-	private Integer tipo;
+	private Integer adjuntoID;
 	private String webpath;
+	private boolean thumb;
+	private String concepto;
 	private String error;
 }
--- src/main/java/com/copsis/services/ImpresionService.java
+++ src/main/java/com/copsis/services/ImpresionService.java
@@ -1,6 +1,8 @@
 package com.copsis.services;
 
 import java.util.Date;
+
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
@@ -36,22 +38,27 @@ public class ImpresionService {
 
 		switch (impresionForm.getTiporespuesta()) {
 		case 1:
-//			Date date = new Date();
-//			Integer year = date.getYear() + 1900;
-//			String mes = (date.getMonth() + 1) + "";
-//			if (mes.length() == 1) {
-//				mes = "0" + mes;
-//			}
-//			String fecha = year.toString().substring(2, 4) + mes;
-//			String nombrePdf = SiO4EncryptorAES.encrypt("Consolidado_" + date,
-//					com.copsis.encryptor.utils.Constants.ENCRYPTION_KEY);
-//			adjuntoForm.setB64(byteArrayPDF.toString());
-//			adjuntoForm.setFolder(folder);
-//			adjuntoForm.setBucket(bucket);
-//			adjuntoForm.setNombreOriginal(nombrePdf);
-//			adjuntoForm.setConcepto(nombrePdf);
-//
-//			adjuntoDTO = quattroUploadClient.getUploadAndAdjuntoByteArray(adjuntoForm, headers).getResult();
+			Date date = new Date();
+			Integer year = date.getYear() + 1900;
+			String mes = (date.getMonth() + 1) + "";
+			if (mes.length() == 1) {
+				mes = "0" + mes;
+			}
+			String fecha = year.toString().substring(2, 4) + mes;
+			String nombrePdf = "";
+			if(impresionForm.getNombreOriginal() != null && !impresionForm.getNombreOriginal().equals("")) {
+				nombrePdf = impresionForm.getNombreOriginal();
+			} else {
+				nombrePdf = SiO4EncryptorAES.encrypt("Consolidado_" + date,
+						com.copsis.encryptor.utils.Constants.ENCRYPTION_KEY);
+			}
+			adjuntoForm.setB64(Base64.encodeBase64String(byteArrayPDF));
+			adjuntoForm.setFolder(folder);
+			adjuntoForm.setBucket(bucket);
+			adjuntoForm.setNombreOriginal(nombrePdf.length() > 50? nombrePdf.substring(0,50):nombrePdf);
+			adjuntoForm.setConcepto(nombrePdf.length() > 50? nombrePdf.substring(0,50):nombrePdf);
+			adjuntoForm.setD(impresionForm.getD());
+			adjuntoDTO = quattroUploadClient.getUploadAndAdjuntoByteArray(adjuntoForm, headers).getResult();
 
 			break;
 
