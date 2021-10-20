808b9d286650155a942adc4d5afac7bd1145f90f
Adan <desarrollo8@copsis.com>
2021-10-20 17:12:28 -0500

    Update MapfreModel.java and MapfreAutosBModel.java

--- src/main/java/com/copsis/models/mapfre/MapfreModel.java
+++ src/main/java/com/copsis/models/mapfre/MapfreModel.java
@@ -5,6 +5,7 @@ import org.apache.pdfbox.text.PDFTextStripper;
 
 import com.copsis.models.DataToolsModel;
 import com.copsis.models.EstructuraJsonModel;
+import com.copsis.models.mapfre.autos.MapfreAutosBModel;
 import com.copsis.models.mapfre.autos.MapfreAutosModel;
 
 public class MapfreModel {
@@ -23,46 +24,72 @@ public class MapfreModel {
 		this.doc = pdDoc;
 		this.contenido = contenido;
 	}
-	
+
 	public EstructuraJsonModel procesa() {
 
-
 		try {
-			  if (contenido.contains("AUTOMOVILES") || contenido.contains(" MOTOCICLETAS") || contenido.contains("SEGURO AUTOPLUS")) {	              
-	                if (contenido.contains("TIPO DE DOCUMENTO:") && contenido.contains("Nombre del agente:")) {
-	                    pagFin = fn.pagFinRango(stripper, doc, "Coberturas Amparadas");
-	                    if (pagFin > 0) {	                    
-	                       // model = new DatosMapfreAutos2(identifica.caratula(1, pagFin, stripper, doc), identifica.recibos(stripper, doc, "Serie de recibo:")).procesar();
-	                   
-	                    	modelo = new MapfreAutosModel(fn.caratula(1, pagFin, stripper, doc),fn.textoBusqueda(stripper, doc, "Serie de recibo:", false) ).procesar();
-	                    }
-	                } else {
-	                    pagFin = fn.pagFinRango(stripper, doc, "INFORMACIÓN ADICIONAL");
-	                    if (pagFin > 0) {
-	                    	modelo = new MapfreAutosModel(fn.caratula(1, pagFin, stripper, doc),"").procesar();
-	                    }
-	                }
-	            }
-			  if (contenido.contains("END CAMBIO DE AGENT") ||  contenido.contains("VIDA INDIVIDUAL") || contenido.contains("PREVICANCER") || contenido.contains("FUNERARIOS") ||  contenido.contains("SEGURO DE VIDA")) { //VIDA	            
-                    modelo  = new MapfreVidaModel(fn.caratula(1, 5, stripper, doc)).procesar();
-              
-                }
-			  
-			  if(contenido.contains("GASTOS MÉDICOS")  || contenido.contains("PROTECCION MEDICA A TU MEDIDA")) {
-			        modelo  = new MapfreSaludModel(fn.caratula(1, 5, stripper, doc)).procesar();
-			  }
-			  
-			  
-			  if (contenido.contains("MULTIPLE EMPRESARIAL") || contenido.contains("SEGUPYME") || contenido.contains("HOGAR")) { //EMPRESARIAL
-	              
-				  modelo = new MapfreDiversosModel(fn.caratula(1, 2, stripper, doc), fn.textoBusqueda(stripper, doc, "Serie de recibo:",false), fn.textoBusqueda(stripper, doc, "ESPECIFICACION DE BIENES CUBIERTOS PARA",false)).procesar();
-	            } 
+			switch (fn.tipoPoliza(contenido)) {
+			case 1:// Autos
+
+				if (contenido.contains("CONDUCTOR HABITUAL")) {
+					modelo = new MapfreAutosBModel(fn.caratula(1, 3, stripper, doc)).procesar();
+				} else {
+					pagFin = fn.pagFinRango(stripper, doc, "Coberturas Amparadas");
+					modelo = new MapfreAutosModel(fn.caratula(1, pagFin, stripper, doc),
+							fn.textoBusqueda(stripper, doc, "Serie de recibo:", false)).procesar();
+				}
+				break;
+			}
+			
+			
+			
+			
+//			if (contenido.contains("AUTOMOVILES") || contenido.contains(" MOTOCICLETAS")
+//					|| contenido.contains("SEGURO AUTOPLUS")) {
+//				if (contenido.contains("TIPO DE DOCUMENTO:") && contenido.contains("Nombre del agente:")) {
+//					pagFin = fn.pagFinRango(stripper, doc, "Coberturas Amparadas");
+//					if (pagFin > 0) {
+//						
+//						if (contenido.contains("CONDUCTOR HABITUAL")) {
+//							System.out.println("paso 1");
+//							modelo = new MapfreAutosBModel(fn.caratula(1, 3, stripper, doc)).procesar();
+//						} else {
+//							modelo = new MapfreAutosModel(fn.caratula(1, pagFin, stripper, doc),fn.textoBusqueda(stripper, doc, "Serie de recibo:", false)).procesar();
+//						}
+//
+//					}
+//				} else {
+//
+//					pagFin = fn.pagFinRango(stripper, doc, "INFORMACIÓN ADICIONAL");
+//					if (pagFin > 0) {
+//						modelo = new MapfreAutosModel(fn.caratula(1, pagFin, stripper, doc), "").procesar();
+//					}
+//				}
+//			}
+//			if (contenido.contains("END CAMBIO DE AGENT") || contenido.contains("VIDA INDIVIDUAL")
+//					|| contenido.contains("PREVICANCER") || contenido.contains("FUNERARIOS")
+//					|| contenido.contains("SEGURO DE VIDA")) { // VIDA
+//				modelo = new MapfreVidaModel(fn.caratula(1, 5, stripper, doc)).procesar();
+//
+//			}
+//
+//			if (contenido.contains("GASTOS MÉDICOS") || contenido.contains("PROTECCION MEDICA A TU MEDIDA")) {
+//				modelo = new MapfreSaludModel(fn.caratula(1, 5, stripper, doc)).procesar();
+//			}
+//
+//			if (contenido.contains("MULTIPLE EMPRESARIAL") || contenido.contains("SEGUPYME")
+//					|| contenido.contains("HOGAR")) { // EMPRESARIAL
+//
+//				modelo = new MapfreDiversosModel(fn.caratula(1, 2, stripper, doc),
+//						fn.textoBusqueda(stripper, doc, "Serie de recibo:", false),
+//						fn.textoBusqueda(stripper, doc, "ESPECIFICACION DE BIENES CUBIERTOS PARA", false)).procesar();
+//			}
 
 			return modelo;
 		} catch (Exception ex) {
 			modelo.setError(
 					MapfreModel.this.getClass().getTypeName() + " - catch:" + ex.getMessage() + " | " + ex.getCause());
-			
+
 			return modelo;
 		}
 	}
