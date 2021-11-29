202238111e2d343cd549c95175b49686890fbe79
Adan <desarrollo8@copsis.com>
2021-11-29 10:45:07 -0600

    Update HdiAutosModel.java

--- src/main/java/com/copsis/models/hdi/HdiAutosModel.java
+++ src/main/java/com/copsis/models/hdi/HdiAutosModel.java
@@ -169,7 +169,7 @@ public class HdiAutosModel {
 						modelo.setPlan(newcontenido.split("\n")[i].split("Paquete:")[1].split("###")[0].trim());
 
 					}
-					System.out.println(newcontenido);
+
 					if (newcontenido.split("\n")[i].contains("ANUAL") || newcontenido.split("\n")[i].contains("12 MESES SIN INTERESES")) {
 						modelo.setFormaPago(1);
 					}
