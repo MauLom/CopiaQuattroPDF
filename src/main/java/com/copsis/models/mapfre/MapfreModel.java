0be2b33ae1a46e96f6f0fb49a0ffc940cfa05f98
Elizabeth Gutierrez <desarrollo23@copsis.com>
2022-02-08 17:27:06 -0600

    Se quitan system

--- src/main/java/com/copsis/models/mapfre/MapfreModel.java
+++ src/main/java/com/copsis/models/mapfre/MapfreModel.java
@@ -76,10 +76,8 @@ public class MapfreModel {
 			case 5://vida
 				if(contenido.contains("ACCIDENTES PERSONALES")) {
 					 modelo = new MapfreVidaCModel(fn.caratula(1, 5, stripper, doc)).procesar();
-					 System.out.println("MapfreVidaCModel");
 				}else {
 					 modelo = new MapfreVidaBModel(fn.caratula(1, 5, stripper, doc)).procesar();
-					 System.err.println("MapfreVidaBModel");
 				}
 				break;
 			}
