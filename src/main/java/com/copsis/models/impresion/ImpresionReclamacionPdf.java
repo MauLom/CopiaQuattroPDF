9f174d3cd858b9a2334d37caf9cf09607acb3311
Adan <desarrollo8@copsis.com>
2022-02-09 12:37:28 -0600

    Update ImpresionReclamacionPdf.java

--- src/main/java/com/copsis/models/impresion/ImpresionReclamacionPdf.java
+++ src/main/java/com/copsis/models/impresion/ImpresionReclamacionPdf.java
@@ -464,8 +464,7 @@ public class ImpresionReclamacionPdf {
 						pdfMerger.mergeDocuments();
 						}
 						
-					} catch (Exception ex) {
-						ex.printStackTrace();
+					} catch (Exception ex) {					
 						throw new GeneralServiceException("00001",
 								"Ocurrio un error en el servicio ImpresionInter: " + ex.getMessage());
 					}
