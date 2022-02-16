a8bd6d9b02fb113840e3e5af06c3e5fb66eb1ac3
Adan <desarrollo8@copsis.com>
2022-02-15 23:24:45 -0600

    Update CoberturaProjection.java

---
+++ src/main/java/com/copsis/clients/projections/CoberturaProjection.java
@@ -0,0 +1,22 @@
+package com.copsis.clients.projections;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonInclude(Include.NON_NULL)
+public class CoberturaProjection {
+	
+	private String nombres;
+	private String sa;
+	private String prima;
+	
+	
+	public CoberturaProjection() {
+		
+	}
+}
