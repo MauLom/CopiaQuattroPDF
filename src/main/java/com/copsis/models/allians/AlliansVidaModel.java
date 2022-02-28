4e5df1d49dd9c18149c3272bbd847a175269e684
Adan <desarrollo8@copsis.com>
2022-02-28 09:29:50 -0600

    Update AlliansVidaModel.java

---
+++ src/main/java/com/copsis/models/allians/AlliansVidaModel.java
@@ -0,0 +1,17 @@
+package com.copsis.models.allians;
+
+import com.copsis.models.DataToolsModel;
+import com.copsis.models.EstructuraJsonModel;
+
+public class AlliansVidaModel {
+	private DataToolsModel fn = new DataToolsModel();
+	private EstructuraJsonModel modelo = new EstructuraJsonModel();
+	private String contenido = "";
+	
+	
+	public AlliansVidaModel(Strinh ) {
+		
+	}
+	
+
+}
