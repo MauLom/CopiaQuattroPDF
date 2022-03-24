7ff1d7ff31cbd1373461b1d7fd4b298d852b0fdc
Adan <desarrollo8@copsis.com>
2022-03-24 10:24:59 -0600

    Desarrollo de nuevo poliza de seguros el potosi

--- src/main/java/com/copsis/services/IdentificaPolizaService.java
+++ src/main/java/com/copsis/services/IdentificaPolizaService.java
@@ -62,6 +62,7 @@ public class IdentificaPolizaService {
 			String contenido = pdfStripper.getText(pdDoc);
 			String contenidoAux = "";		
 			boolean encontro = false;
+		
 	      
 			// CHUBB
 			if (!encontro && contenido.contains("Chubb")) {
@@ -314,7 +315,7 @@ public class IdentificaPolizaService {
 			
 			
 		
-		    if (!encontro && contenido.contains("Seguros el Potosí S.A.")){		    			    		                
+		    if (!encontro && (contenido.contains("Seguros el Potosí S.A.") || contenido.contains("www.elpotosi.com.mx"))){		    			    		                
                 	PotosiModel datospotosi = new PotosiModel(pdfStripper, pdDoc, contenido);
                 	modelo = datospotosi.procesar();
                     encontro = true;                
