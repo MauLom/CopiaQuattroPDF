cc29fede01c94cf9307c4eae8255d7ec57939d73
Adan <desarrollo8@copsis.com>
2022-03-30 09:15:46 -0600

    Update AguilaAutosModel.java

--- src/main/java/com/copsis/models/aguila/AguilaAutosModel.java
+++ src/main/java/com/copsis/models/aguila/AguilaAutosModel.java
@@ -32,18 +32,19 @@ public class AguilaAutosModel {
 			 newcontenido.append( fn.extracted(inicio, fin, contenido));
 			 
 			 for (int i = 0; i < newcontenido.toString().split("\n").length; i++) {
-			
+				 
 				 if(newcontenido.toString().split("\n")[i].split("-").length  > 3 && fn.obtenVigePoliza(newcontenido.toString().split("\n")[i]).size() == 3) {					
 					 modelo.setFechaEmision(fn.formatDateMonthCadena(fn.obtenVigePoliza(newcontenido.toString().split("\n")[i]).get(0)));
 					 modelo.setVigenciaDe(fn.formatDateMonthCadena(fn.obtenVigePoliza(newcontenido.toString().split("\n")[i]).get(1)));
 					 modelo.setVigenciaA(fn.formatDateMonthCadena(fn.obtenVigePoliza(newcontenido.toString().split("\n")[i]).get(2)));
 					 modelo.setCteNombre(newcontenido.toString().split("\n")[i].split("###")[0].trim());
+					 modelo.setPoliza(newcontenido.toString().split("\n")[i].split("###")[5].trim());
 					 
 				 }
 				 if(newcontenido.toString().split("\n")[i].contains("Plan Contratado") && newcontenido.toString().split("\n")[i].contains("Forma de Pago") && newcontenido.toString().split("\n")[i].contains("Agente")) {
 					 modelo.setPlan(newcontenido.toString().split("\n")[i+1].split("###")[0]);
 					 modelo.setFormaPago(fn.formaPagoSring(newcontenido.toString().split("\n")[i+1]));
-					 modelo.setClave(newcontenido.toString().split("\n")[i+1].split("###")[newcontenido.toString().split("\n")[i+1].split("###").length-1].trim());
+					 modelo.setCveAgente(newcontenido.toString().split("\n")[i+1].split("###")[newcontenido.toString().split("\n")[i+1].split("###").length-1].trim());
 				 }
 				 if(newcontenido.toString().split("\n")[i].contains("Calle") && newcontenido.toString().split("\n")[i].contains("Colonia")) {
 					 modelo.setCteDireccion(newcontenido.toString().split("\n")[i+1].replace("###", " ").trim());
@@ -113,17 +114,42 @@ public class AguilaAutosModel {
 			 inicio = contenido.indexOf("COBERTURAS BÁSICAS");
 			 fin = contenido.indexOf("Hoja 1");
 			 newcontenido = new StringBuilder();
-			 newcontenido.append( fn.extracted(inicio, fin, contenido));
+			 newcontenido.append( fn.extracted(inicio, fin, contenido)
+					 .replace("Responsabilidad ###Civil ###por ###Daños ###a", "Responsabilidad Civil por Daños a terceros en sus bienes o sus ###1,000,000.00")
+					 .replace("Equipo Especial, Adaptaciones y", "Equipo Especial, Adaptaciones y Conversiones###EXCLUIDO")
+					 .replace("Gastos Médicos a Ocupantes", "Gastos Médicos a Ocupantes###220,000.00")
+					 .replace("Responsabilidad Civil Catastrófica", "Responsabilidad Civil Catastrófica###3,000,000.00"));
 			 
 			 List<EstructuraCoberturasModel> coberturas = new ArrayList<>();
 			 for (int i = 0; i < newcontenido.toString().split("\n").length; i++) {
 				 EstructuraCoberturasModel cobertura = new EstructuraCoberturasModel();
 				
 				 if(!newcontenido.toString().split("\n")[i].contains("COBERTURAS BÁSICAS") && !newcontenido.toString().split("\n")[i].contains("Cobertura")) {
-					 //System.out.println(newcontenido.toString().split("\n")[i] +"----< " + newcontenido.toString().split("\n")[i].split("###").length);
+					 
+					 
+					 
+					 switch (newcontenido.toString().split("\n")[i].split("###").length) {
+					 case 2:
+							cobertura.setNombre(newcontenido.toString().split("\n")[i].split("###")[0]);
+							cobertura.setSa(newcontenido.toString().split("\n")[i].split("###")[1]);							
+							coberturas.add(cobertura);
+							
+							break;
+					 case 3:
+						cobertura.setNombre(newcontenido.toString().split("\n")[i].split("###")[0]);
+						cobertura.setSa(newcontenido.toString().split("\n")[i].split("###")[1]);
+						cobertura.setDeducible(newcontenido.toString().split("\n")[i].split("###")[2]);
+						coberturas.add(cobertura);
+						
+						break;
+				
+					default:
+						break;
+					}
 				 }
 			 }
-			 
+			    modelo.setMoneda(1);
+				modelo.setCoberturas(coberturas);
 			return modelo;
 		} catch (Exception ex) {
 			modelo.setError(AguilaAutosModel.this.getClass().getTypeName() + " - catch:" + ex.getMessage() + " | "
