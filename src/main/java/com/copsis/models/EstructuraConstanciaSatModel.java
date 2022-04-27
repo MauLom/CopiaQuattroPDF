d47f1473e9ff04b73495fd288df8aaf8686de88a
Adan <desarrollo8@copsis.com>
2022-04-27 15:10:30 -0500

    codigo de extracion de datos del sat

---
+++ src/main/java/com/copsis/models/EstructuraConstanciaSatModel.java
@@ -0,0 +1,43 @@
+package com.copsis.models;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonInclude(Include.NON_NULL)
+public class EstructuraConstanciaSatModel {
+
+	 private  String rfc="";
+	 private  String curp="";
+	 private  String nombre="";
+	 private  String apellidoP="";
+	 private  String apellidoM="";
+	 private  String fechaOperaciones="";
+	 private  String statusPadron="";
+	 private  String fechaEstado="";
+	 private  String nombreComercial="";
+	 private  String cp="";	
+	 private  String tipoVialidad="";
+	 private  String nombreVialidad="";
+	 private  String numeroExterior="";
+	 private  String numeroInterior="";
+	 private  String colonia="";
+	 private  String localidad="";
+	 private  String municipio="";
+	 private  String estado="";
+	 private  String entreCalle="";
+	 private  String yCalle="";
+	 private  String correo="";
+	 private  String telefonoLada="";
+	 private  String movilLada="";
+	 private  String telefonoFijo="";
+	 private  String telefonoMovil="";
+	 private  String estadoDomicilio="";
+	 private  String estadoContribuyente="";
+	 private  String error="";
+}
+	
