92a2fa82cab673a800d50440ec41189655bdf3ed
Adan <desarrollo8@copsis.com>
2022-05-25 14:52:12 -0500

    Update Folio.java

--- src/main/java/com/copsis/controllers/forms/Folio.java
+++ src/main/java/com/copsis/controllers/forms/Folio.java
@@ -1,10 +1,9 @@
 package com.copsis.controllers.forms;
 
-import javax.validation.constraints.NotBlank;
+
 
 import lombok.Getter;
 @Getter
 public class Folio {
-	@NotBlank(message = "¡El parametro 'folio' no puede ir vacio!")
 	private	String folio;
 }
