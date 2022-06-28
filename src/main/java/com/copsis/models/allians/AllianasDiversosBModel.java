8462f513817863ec543f38735a3af7d5b5f7c791
Adan <desarrollo8@copsis.com>
2022-06-28 17:00:07 -0500

    Update AllianasDiversosBModel.java

--- src/main/java/com/copsis/models/allians/AllianasDiversosBModel.java
+++ src/main/java/com/copsis/models/allians/AllianasDiversosBModel.java
@@ -1,7 +1,11 @@
 package com.copsis.models.allians;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.copsis.models.DataToolsModel;
 import com.copsis.models.EstructuraJsonModel;
+import com.copsis.models.EstructuraUbicacionesModel;
 
 public class AllianasDiversosBModel {
 	private DataToolsModel fn = new DataToolsModel();
@@ -9,6 +13,7 @@ public class AllianasDiversosBModel {
 	private String contenido = "";
 	
 	public  EstructuraJsonModel procesar(String contenido) {
+		StringBuilder direccion = new StringBuilder();
 		StringBuilder newcontenido = new StringBuilder();
 		int inicio = 0;
 		int fin = 0;		
@@ -22,27 +27,65 @@ public class AllianasDiversosBModel {
 			fin = contenido.indexOf("Datos de la ubicación Asegurada");
 			newcontenido.append( fn.extracted(inicio, fin, contenido));
 			for (int i = 0; i < newcontenido.toString().split("\n").length; i++) {
-				
+			
 				if(newcontenido.toString().split("\n")[i].contains("Razon Social")) {
 					modelo.setCteNombre(newcontenido.toString().split("\n")[i+1].split("###")[0]);	
 				}
 				if(newcontenido.toString().split("\n")[i].split("-").length > 3) {
-					System.out.println("=========> " +newcontenido.toString().split("\n")[i]);
-					System.out.println(fn.obtenVigePoliza(newcontenido.toString().split("\n")[i]));
+					modelo.setPoliza(newcontenido.toString().split("\n")[i].split("###")[0]);
+					modelo.setMoneda(fn.buscaMonedaEnTexto(newcontenido.toString().split("\n")[i]));
+					modelo.setFormaPago(fn.formaPagoSring(newcontenido.toString().split("\n")[i]));
 					modelo.setFechaEmision(fn.formatDateMonthCadena(fn.obtenVigePoliza(newcontenido.toString().split("\n")[i]).get(0)));
 					modelo.setVigenciaDe(fn.formatDateMonthCadena(fn.obtenVigePoliza(newcontenido.toString().split("\n")[i]).get(1)));
-					modelo.setVigenciaA(fn.formatDateMonthCadena(fn.obtenVigePoliza(newcontenido.toString().split("\n")[i]).get(2)));
-					
+					modelo.setVigenciaA(fn.formatDateMonthCadena(fn.obtenVigePoliza(newcontenido.toString().split("\n")[i]).get(2)));					
 				}
-				if(newcontenido.toString().split("\n")[i].contains("Razon Social")) {
-					
+				if(newcontenido.toString().split("\n")[i].contains("Agente:")) {					
+					modelo.setCveAgente(fn.numTx(newcontenido.toString().split("\n")[i].split("Agente:")[1].replace("###", "")));
+					modelo.setAgente(newcontenido.toString().split("\n")[i].split("Agente:")[1].split(modelo.getCveAgente())[1].replace("###", ""));
 				}
-				if(newcontenido.toString().split("\n")[i].contains("Razon Social")) {
-					
+				if(newcontenido.toString().split("\n")[i].contains("RFC")) {
+					modelo.setRfc(newcontenido.toString().split("\n")[i].split("RFC:")[1].replace("###", "").trim());
+				}
+				if(newcontenido.toString().split("\n")[i].contains("Calle")) {
+					direccion.append(newcontenido.toString().split("\n")[i].split("Calle:")[1].replace("###", "").trim());
+				}
+				if(newcontenido.toString().split("\n")[i].contains("Colonia")) {
+					direccion.append(newcontenido.toString().split("\n")[i].split("Colonia:")[1].replace("###", "").trim());
+				}
+				if(newcontenido.toString().split("\n")[i].contains("Población")) {
+					direccion.append("  " +newcontenido.toString().split("\n")[i].split("Población:")[1].replace("###", "").trim() +" ");
+				}				
+				if(newcontenido.toString().split("\n")[i].contains("País")) {
+	        		direccion.append(newcontenido.toString().split("\n")[i].split("País:")[1].replace("###", "").trim());
+				}
+				if(newcontenido.toString().split("\n")[i].contains("Estado")) {
+					direccion.append(newcontenido.toString().split("\n")[i].split("Estado:")[1].replace("###", "").trim());				
+				}				
+				if(newcontenido.toString().split("\n")[i].contains("C.P:")) {
+	        		modelo.setCp(newcontenido.toString().split("\n")[i].split("C.P:")[1].replace("###", "").trim());
+				}				
+			}			
+			modelo.setCteDireccion(direccion.toString());
+			
+			
+//			 System.out.println(contenido);
+			inicio = contenido.indexOf("Datos de la ubicación Asegurada");
+			fin = contenido.indexOf("Año Construcción");
+			newcontenido = new StringBuilder();
+			newcontenido.append( fn.extracted(inicio, fin, contenido));
+			List<EstructuraUbicacionesModel> ubicaciones = new ArrayList<>();
+			EstructuraUbicacionesModel ubicacion = new EstructuraUbicacionesModel();
+			
+			for (int i = 0; i < newcontenido.toString().split("\n").length; i++) {
+				System.out.println(newcontenido.toString().split("\n")[i]);
+				if(newcontenido.toString().split("\n")[i].contains("calle")) {
+				ubicacion.setCalle(newcontenido.toString().split("\n")[i].split("calle")[1].replace("###", "").trim());	
+				}
+				
+				if(newcontenido.toString().split("\n")[i].contains("Tipo Muros:") &&  newcontenido.toString().split("\n")[i].contains("Tipo Muros:")) {
+					System.out.println("casasa==========>" +fn.material(newcontenido.toString().split("\n")[i].split("Muros:")[1].split("Tipo Techo:")[0].replace("###", "")));
 				}
 			}
-			
-			
 		
 			return modelo;
 		} catch (Exception ex) {
