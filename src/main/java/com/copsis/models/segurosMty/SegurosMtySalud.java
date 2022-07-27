b9a89cb26820b92b0f9e697e0c97d3945c484ab6
Jorge Reyes <jreyes@LAP-UBU20-JorgeR>
2022-07-27 10:45:40 -0500

    primas de primer recibo y recibo subsecuente

--- src/main/java/com/copsis/models/segurosMty/SegurosMtySalud.java
+++ src/main/java/com/copsis/models/segurosMty/SegurosMtySalud.java
@@ -188,6 +188,12 @@ public class SegurosMtySalud {
 	        			if(newcontenido.split("\n")[i].contains("TOTAL")) {
 	        				modelo.setPrimaTotal(fn.castBigDecimal(fn.cleanString( newcontenido.split("\n")[i].split("TOTAL")[1].split("###")[1].trim())));
 	        			}
+	        			if(newcontenido.split("\n")[i].contains("PRIMER RECIBO") && newcontenido.split("\n")[i].contains("RECARGO")) {
+	        				modelo.setPrimerPrimatotal(fn.castBigDecimal(fn.cleanString(fn.gatos(newcontenido.split("\n")[i].split("RECARGO")[0].replaceAll("[A-Z]", "")))));
+	        			}
+	        			if(newcontenido.split("\n")[i].contains("RECIBOS SUBSECUENTES") && newcontenido.split("\n")[i].contains("IVA")) {
+	        				modelo.setSubPrimatotal(fn.castBigDecimal(fn.cleanString(fn.gatos(newcontenido.split("\n")[i].split("IVA")[0].replaceAll("[A-Z]", "")))));
+	        			}	        			
 	        			
 	        		}
 	         }
