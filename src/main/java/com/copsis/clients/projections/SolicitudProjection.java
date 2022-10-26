e1b0b35319b3cb8014d6a77597981dc61d01121c
Adan Albanil Cruz <aalbanil@AIOU-UBU10-AdanA>
2022-10-26 11:05:02 -0500

    Actualizar ContratanteProjection.java, MenorProjection.java y 3 más archivos...

--- src/main/java/com/copsis/clients/projections/SolicitudProjection.java
+++ src/main/java/com/copsis/clients/projections/SolicitudProjection.java
@@ -10,11 +10,11 @@ import lombok.Setter;
 @Setter
 @JsonInclude(Include.NON_NULL)
 public class SolicitudProjection {
-	  private String pregunta1;
-	  private String pregunta2;
-	  private String pregunta2R1;
-	  private String pregunta3;
-	  private String pregunta3R1;
-	  private Boolean pregunta4;
-	  private String pregunta5;
+    private String pregunta1;
+    private String pregunta2;
+    private String pregunta2R1;
+    private String pregunta3;
+    private String pregunta3R1;
+    private Boolean pregunta4;
+    private String pregunta5;
 }
--- src/main/java/com/copsis/controllers/forms/ImpresionAxaVidaForm.java
+++ src/main/java/com/copsis/controllers/forms/ImpresionAxaVidaForm.java
@@ -1,8 +1,5 @@
 package com.copsis.controllers.forms;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
-
 import lombok.Getter;
 import lombok.Setter;
 
