b4c1c6d18dadee1058e4e3d81a04ae38fd6b600f
Adan Albanil Cruz <aalbanil@AIOU-UBU10-AdanA>
2022-12-20 11:39:20 -0600

    Actualizar BexmasSaludModel.java

--- src/main/java/com/copsis/models/bexmas/BexmasSaludModel.java
+++ src/main/java/com/copsis/models/bexmas/BexmasSaludModel.java
@@ -322,7 +322,6 @@ public class BexmasSaludModel {
 			
 			return modelo;
 		} catch (Exception ex) {
-			ex.printStackTrace();
 			modelo.setError(BexmasSaludModel.this.getClass().getTypeName() + " - catch:" + ex.getMessage() + " | "
 					+ ex.getCause());
 			return modelo;
