7a5bf471acd07c58add5196b014e505b6b77d594
Adan Albanil Cruz <aalbanil@AIOU-UBU10-AdanA>
2023-04-27 12:10:53 -0600

    Actualizar CertificadoProjection.java

--- src/main/java/com/copsis/clients/projections/CertificadoProjection.java
+++ src/main/java/com/copsis/clients/projections/CertificadoProjection.java
@@ -6,7 +6,24 @@ import lombok.Getter;
 @Getter
 @JsonInclude(value = Include.NON_NULL)
 public class CertificadoProjection {
-    private	String  nombre;
-	private	String vigencia;
+    private	String  numeroPoliza;
+	private	String fechaEmision;
+    private	String fechaDesde;
+    private	String fechaHasta;
+    private	String nombre;
+    private	String app;
+    private	String apm;
+    private	String telefono;
+    private	String domicilio;
+    private	String ciudad;
+    private	String cp;
+    private	String estado;
+    private	String formaPago;
+    private	String modelo;
+    private	String marca;
+    private	String descripcion;
+    private	String serie;
+    private	String placas;
+    private	String uso;
     
 }
