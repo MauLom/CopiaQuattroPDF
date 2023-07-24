4f367cd0a64f8300e6238bf18ae41d43f24223a8
Adan Albanil Cruz <aalbanil@AIOU-UBU10-AdanA>
2023-07-24 13:31:34 -0600

    Actualizar InbursaVidaBModel.java

--- src/main/java/com/copsis/models/inbursa/InbursaVidaBModel.java
+++ src/main/java/com/copsis/models/inbursa/InbursaVidaBModel.java
@@ -1,5 +1,6 @@
 package com.copsis.models.inbursa;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -40,12 +41,23 @@ public class InbursaVidaBModel {
 
 			newcontenido.append(fn.extracted(inicio, fin, contenido).replace("R.F.C:", "R.F.C."));
 			
+			
 			for (int i = 0; i < newcontenido.toString().split("\n").length; i++) {
+				
 		     if(newcontenido.toString().split("\n")[i].contains("PÓLIZA") &&  newcontenido.toString().split("\n")[i].contains("CIS")) {
 		    	modelo.setPoliza(newcontenido.toString().split("\n")[i-1].split("###")[1].trim()); 
 		     }
+
+			  if(newcontenido.toString().split("\n")[i].contains("Póliza") &&  newcontenido.toString().split("\n")[i].contains("CIS")) {
+		    	modelo.setPoliza(newcontenido.toString().split("\n")[i].split("Póliza")[1].split("CIS")[0].replace("###","").trim()); 
+		     }
 		     
 		     if(newcontenido.toString().split("\n")[i].contains("NOMBRE") && newcontenido.toString().split("\n")[i].contains("AGRUPACIÓN") &&  newcontenido.toString().split("\n")[i].contains("PRIMA NETA")) {
+		    	 modelo.setCteNombre(newcontenido.toString().split("\n")[i+1].split("###")[0]);
+		    		modelo.setPrimaneta(fn.castBigDecimal(fn.castDouble(newcontenido.toString().split("\n")[i + 1].split("###")[2])));		    	
+		     }
+			  if(newcontenido.toString().split("\n")[i].contains("Nombre") && newcontenido.toString().split("\n")[i].contains("Agrupación") 
+			  &&  newcontenido.toString().split("\n")[i].contains("Prima neta")) {
 		    	 modelo.setCteNombre(newcontenido.toString().split("\n")[i+1].split("###")[0]);
 		    		modelo.setPrimaneta(fn.castBigDecimal(fn.castDouble(newcontenido.toString().split("\n")[i + 1].split("###")[2])));		    	
 		     }
@@ -68,7 +80,17 @@ public class InbursaVidaBModel {
 				    	modelo.setRfc(newcontenido.toString().split("\n")[i+1].split("###")[1]);
 				    	modelo.setPrimaTotal(fn.castBigDecimal(fn.castDouble(newcontenido.toString().split("\n")[i + 1].split("###")[2])));		    	
 			     }
-			 
+			 if(modelo.getCteDireccion().length() == 0 && newcontenido.toString().split("\n")[i].contains("COL.") && newcontenido.toString().split("\n")[i].contains("C.P.")){
+					newdire.append(newcontenido.toString().split("\n")[i].split("C.P.")[0].replace("###", " "));
+			 }
+
+			 if( newcontenido.toString().split("\n")[i].contains("C.P.")){
+                 List<String> valores = fn.obtenerListNumeros2(newcontenido.toString().split("\n")[i]);
+				if(!valores.isEmpty()){
+                  modelo.setCp(valores.get(0));
+				}
+			 }
+			
 			 
 			 if(newcontenido.toString().split("\n")[i].contains("MONEDA")) {
 				 modelo.setMoneda(fn.buscaMonedaEnTexto(newcontenido.toString().split("\n")[i+1]));
@@ -82,6 +104,15 @@ public class InbursaVidaBModel {
 				modelo.setVigenciaA(fn.formatDateMonthCadena(fn.obtenVigePoliza2(newcontenido.toString().split("\n")[i+1]).get(1)));
 			 }
 
+			  if(newcontenido.toString().split("\n")[i].contains("Desde") &&newcontenido.toString().split("\n")[i].contains("hasta") && newcontenido.toString().split("\n")[i].split("-").length > 3) {				
+				modelo.setVigenciaDe(fn.formatDateMonthCadena(fn.obtenVigePoliza2(newcontenido.toString().split("\n")[i]).get(0)));
+				modelo.setVigenciaA(fn.formatDateMonthCadena(fn.obtenVigePoliza2(newcontenido.toString().split("\n")[i]).get(1)));
+			 }
+
+			}
+
+			if(modelo.getPrimaTotal() == BigDecimal.ZERO){
+            modelo.setPrimaTotal(modelo.getPrimaneta());
 			}
 			
 	     modelo.setCteDireccion(newdire.toString());
@@ -89,18 +120,25 @@ public class InbursaVidaBModel {
 			
 			inicio = contenido.indexOf("Asegurado");
 			fin = contenido.indexOf("ESPECIFICACIÓN DE SECCION");
+			fin = fin == -1? contenido.indexOf("@COBERTURAS"):fin;
+
+			
 			newcontenido = new StringBuilder();
 			newcontenido.append(fn.extracted(inicio, fin, contenido));
+
 			List<EstructuraAseguradosModel> asegurados = new ArrayList<>();
 			for (int i = 0; i < newcontenido.toString().split("\n").length; i++) {		
 				 if(!newcontenido.toString().split("\n")[i].contains("nacimiento") && !newcontenido.toString().split("\n")[i].contains("Asegurado")
 						 && !newcontenido.toString().split("\n")[i].contains("fumador") 
+						 && !newcontenido.toString().split("\n")[i].contains("Nacimiento") 
 						 ) {
-					 EstructuraAseguradosModel asegurado = new EstructuraAseguradosModel();   
-					 asegurado.setNombre(newcontenido.toString().split("\n")[i].split("###")[0]);
-					 asegurado.setParentesco(fn.parentesco(newcontenido.toString().split("\n")[i].split("###")[2]));
-					 asegurado.setNacimiento(fn.formatDateMonthCadena(fn.obtenVigePoliza2(newcontenido.toString().split("\n")[i]).get(0)));
-					 asegurados.add(asegurado);
+							if(newcontenido.toString().split("\n")[i].split("-").length > 2){
+							EstructuraAseguradosModel asegurado = new EstructuraAseguradosModel();   
+							asegurado.setNombre(newcontenido.toString().split("\n")[i].split("###")[0]);							
+							asegurado.setParentesco(fn.parentesco(newcontenido.toString().split("\n")[i].split("###")[2]));
+							 asegurado.setNacimiento(fn.formatDateMonthCadena(fn.obtenVigePoliza2(newcontenido.toString().split("\n")[i]).get(0)));
+							asegurados.add(asegurado);
+					}				
 				 }
 			}
 			modelo.setAsegurados(asegurados);
@@ -153,11 +191,24 @@ public class InbursaVidaBModel {
 					}
 				}
 			}
+			inicio = contenido.indexOf("Opción de pago");
+			fin = contenido.indexOf("LA PRESENTE PÓLIZA QUEDA");
+            newcontenido = new StringBuilder();
+			newcontenido.append(fn.extracted(inicio, fin, contenido));
+			for (int i = 0; i < newcontenido.toString().split("\n").length; i++) {				
+				if(newcontenido.toString().split("\n")[i].contains("sola exhibición")){
+					 modelo.setFormaPago(1);
+					 modelo.setMoneda(1);
+				}
+		       
+			}
+			
+             
 			
 	
 
 			return modelo;
-		} catch (Exception ex) {
+		} catch (Exception ex) {			
 			modelo.setError(InbursaVidaBModel.this.getClass().getTypeName() + " - catch:" + ex.getMessage() + " | "
 					+ ex.getCause());
 			return modelo;
