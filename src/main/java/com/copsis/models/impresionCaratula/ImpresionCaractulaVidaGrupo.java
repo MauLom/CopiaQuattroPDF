a5fb1652e76fcc5759d6dfed4f65365cb43e2887
Adan Albanil Cruz <aalbanil@AIOU-UBU10-AdanA>
2023-09-26 15:08:41 -0600

    Actualizar ImpresionCaractulaVidaGrupo.java

---
+++ src/main/java/com/copsis/models/impresionCaratula/ImpresionCaractulaVidaGrupo.java
@@ -0,0 +1,15 @@
+package com.copsis.models.impresionCaratula;
+
+import com.copsis.models.Tabla.Sio4CommunsPdf;
+import java.awt.Color;
+
+public class ImpresionCaractulaVidaGrupo {
+    
+    private Color blue = new Color(40, 76, 113);
+        private Color black = new Color(0, 0, 0);
+        private Color gray = new Color(229, 234, 237);
+        private float yStart = 780, bottomMargin = 30, fullWidth = 590, margin = 10, ytexto = 0,pivote=0,vPosionY=0;
+        private Sio4CommunsPdf communsPdf = new Sio4CommunsPdf();
+        private boolean drawLines = true;
+        private boolean acumula;
+}
