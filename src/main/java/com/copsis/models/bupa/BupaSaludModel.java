100d3d2d5d5f47fdaabf8050ef83c8978eafd34a
Adan Albanil Cruz <aalbanil@aiou-ubu10-adana>
2023-10-26 15:24:04 -0600

    Ajuste de primas ,y correcion issus.

--- src/main/java/com/copsis/models/bupa/BupaSaludModel.java
+++ src/main/java/com/copsis/models/bupa/BupaSaludModel.java
@@ -1,5 +1,6 @@
 package com.copsis.models.bupa;
 
+import com.copsis.constants.ConstantsValue;
 import com.copsis.models.DataToolsModel;
 import com.copsis.models.EstructuraAseguradosModel;
 import com.copsis.models.EstructuraCoberturasModel;
@@ -21,8 +22,9 @@ public class BupaSaludModel {
 			modelo.setTipo(3);
 			modelo.setCia(87);
 	
+			
 		
-			inicio = contenido.indexOf("Contratante");
+			inicio = contenido.indexOf(ConstantsValue.CONTRATANTE2);
 			fin = contenido.indexOf("Advertencia");
 			fin = fin == -1 ? contenido.indexOf("Área de Cobertur"):fin;
 	
@@ -33,12 +35,12 @@ public class BupaSaludModel {
 			EstructuraCoberturasModel cobertura = new EstructuraCoberturasModel();
 			for (int i = 0; i < newcontenido.toString().split("\n").length; i++) {	
 			
-				if(newcontenido.toString().split("\n")[i].contains("Contratante") && newcontenido.toString().split("\n")[i+1].contains("Dirección")) {				
+				if(newcontenido.toString().split("\n")[i].contains(ConstantsValue.CONTRATANTE2) && newcontenido.toString().split("\n")[i+1].contains("Dirección")) {				
 					
-					modelo.setCteNombre(newcontenido.toString().split("\n")[i].split("Contratante")[1].replace("###", "").trim());
+					modelo.setCteNombre(newcontenido.toString().split("\n")[i].split(ConstantsValue.CONTRATANTE2)[1].replace("###", "").trim());
 				}
-				if(newcontenido.toString().split("\n")[i].contains("Dirección")) {
-					modelo.setCteDireccion(newcontenido.toString().split("\n")[i].split("Dirección")[1].replace("###", "").trim());					
+				if(newcontenido.toString().split("\n")[i].contains(ConstantsValue.DIRECCIONMC)) {
+					modelo.setCteDireccion(newcontenido.toString().split("\n")[i].split(ConstantsValue.DIRECCIONMC)[1].replace("###", "").trim());					
 					modelo.setCp(fn.obtenerCPRegex2(newcontenido.toString().split("\n")[i+1]));
 					if(modelo.getCp().length() == 0) {
 					    modelo.setCp(fn.obtenerCPRegex2(newcontenido.toString().split("\n")[i+2]));
@@ -46,12 +48,12 @@ public class BupaSaludModel {
 				}
 			
 				if(newcontenido.toString().split("\n")[i].contains("Número de Póliza")) {
-					modelo.setPoliza(newcontenido.toString().split("\n")[i].split("Póliza")[1].replace("###", "").trim());
+					modelo.setPoliza(newcontenido.toString().split("\n")[i].split(ConstantsValue.POLIZA_ACENT)[1].replace("###", "").trim());
 				}
-				if(newcontenido.toString().split("\n")[i].contains("Número de")&& newcontenido.toString().split("\n")[i+1].contains("Póliza") ) {
-					modelo.setPoliza(newcontenido.toString().split("\n")[i].split("Número de")[1].replace("###", "").trim());
+				if(newcontenido.toString().split("\n")[i].contains(ConstantsValue.NUMERODE)&& newcontenido.toString().split("\n")[i+1].contains(ConstantsValue.POLIZA_ACENT) ) {
+					modelo.setPoliza(newcontenido.toString().split("\n")[i].split(ConstantsValue.NUMERODE)[1].replace("###", "").trim());
 				}
-				if(newcontenido.toString().split("\n")[i].contains("Vigencia")  && newcontenido.toString().split("\n")[i].contains("Vigencia")) {
+				if(newcontenido.toString().split("\n")[i].contains("Vigencia")) {
 					modelo.setVigenciaDe(fn.formatDateMonthCadena(fn.obtenVigePoliza(newcontenido.toString().split("\n")[i]).get(0)));
 				}
 				if(newcontenido.toString().split("\n")[i].contains("Hasta")) {
@@ -68,9 +70,9 @@ public class BupaSaludModel {
 				}
 				if(newcontenido.toString().split("\n")[i].contains("Fraccionado")) {
 					  List<String> valores = fn.obtenerListNumeros(newcontenido.toString().split("\n")[i]);
-				if(valores.size() > 0) {
+					if(!valores.isEmpty()) {
 					  modelo.setRecargo(fn.castBigDecimal(fn.castDouble(valores.get(0))));
-				}
+					}
 				}
 				if(newcontenido.toString().split("\n")[i].contains("IVA")) {
 					  List<String> valores = fn.obtenerListNumeros(newcontenido.toString().split("\n")[i]);
@@ -79,6 +81,19 @@ public class BupaSaludModel {
 				if(newcontenido.toString().split("\n")[i].contains("Total")) {
 					  List<String> valores = fn.obtenerListNumeros(newcontenido.toString().split("\n")[i]);
 					  modelo.setPrimaTotal(fn.castBigDecimal(fn.castDouble(valores.get(0))));
+				}
+				
+				if(newcontenido.toString().split("\n")[i].contains("Cónyuge")) {					
+					  List<String> valores = fn.obtenerListNumeros(newcontenido.toString().split("\n")[i]);
+					  modelo.setCargoExtra(fn.castBigDecimal(fn.castDouble(valores.get(0))));
+					  
+				}
+				
+				
+				if(newcontenido.toString().split("\n")[i].contains("based discount")) {					
+					  List<String> valores = fn.obtenerListNumeros(newcontenido.toString().split("\n")[i]);
+					  modelo.setAjusteUno(fn.castBigDecimal(fn.castDouble(valores.get(0))));
+
 				}
 				
 				if(newcontenido.toString().split("\n")[i].contains("Asegurado Principal") || newcontenido.toString().split("\n")[i].contains("Asegurado Titular")) {				
@@ -117,7 +132,7 @@ public class BupaSaludModel {
 			
 			
 	
-			inicio = conteniext.indexOf("Forma de Pago");
+			inicio = conteniext.indexOf(ConstantsValue.FORMA_PAGO2);
 			fin = conteniext.indexOf("Vigencia###Prima Neta");
 			
 			if(fin == -1) {
@@ -132,37 +147,36 @@ public class BupaSaludModel {
 			
 			for (int i = 0; i < newcontenido.toString().split("\n").length; i++) {			
 				
-			if(newcontenido.toString().split("\n")[i].contains("Forma de Pago")) {
+			if(newcontenido.toString().split("\n")[i].contains(ConstantsValue.FORMA_PAGO2) ) {
 				
 				  modelo.setFormaPago( fn.formaPagoSring(newcontenido.toString().split("\n")[i] ));
 				}		
-				if(modelo.getFormaPago() == 0 && newcontenido.toString().split("\n")[i].contains("Forma de Pago")) {
+				if(modelo.getFormaPago() == 0 && newcontenido.toString().split("\n")[i].contains(ConstantsValue.FORMA_PAGO2)) {
 				  modelo.setFormaPago( fn.formaPagoSring(newcontenido.toString().split("\n")[i+1] ));
 				}
 				
-				if(newcontenido.toString().split("\n")[i].contains("Moneda")) {
-					if(newcontenido.toString().split("\n")[i+1].contains("MXP")) {
+				if(newcontenido.toString().split("\n")[i].contains("Moneda") && newcontenido.toString().split("\n")[i+1].contains("MXP")) {
+				
 						modelo.setMoneda(1);
-					}
+					
 				}
 					
 			}
 			if(modelo.getFormaPago() == 0) {		
-				inicio = recibo.indexOf("Forma de Pago");
+				inicio = recibo.indexOf(ConstantsValue.FORMA_PAGO2);
 				fin = recibo.indexOf("Prima Neta");
 				newcontenido.append(fn.extracted(inicio, fin, recibo));
 				
 				for (int i = 0; i < newcontenido.toString().split("\n").length; i++) {
-					if (newcontenido.toString().split("\n")[i].contains("Forma de Pago")) {
+					if (newcontenido.toString().split("\n")[i].contains(ConstantsValue.FORMA_PAGO2)) {
 						modelo.setFormaPago(fn.formaPagoSring(newcontenido.toString().split("\n")[i + 1]));
 					}
-					if (newcontenido.toString().split("\n")[i].contains("Forma de Pago")) {
+					if (newcontenido.toString().split("\n")[i].contains(ConstantsValue.FORMA_PAGO2)) {
 						modelo.setFormaPago(fn.formaPagoSring(newcontenido.toString().split("\n")[i]));
 					}
-					if (newcontenido.toString().split("\n")[i].contains("Moneda")) {
-						if (newcontenido.toString().split("\n")[i + 1].contains("MXP")) {
+					if (newcontenido.toString().split("\n")[i].contains("Moneda") && newcontenido.toString().split("\n")[i + 1].contains("MXP")) {						
 							modelo.setMoneda(1);
-						}
+						
 					}
 				}
 			}
@@ -179,7 +193,8 @@ public class BupaSaludModel {
 	
 			
 			return modelo;
-		} catch (Exception ex) {			
+		} catch (Exception ex) {	
+			
 			modelo.setError(BupaSaludModel.this.getClass().getTypeName() + " | " + ex.getMessage() + " | " + ex.getCause());
 			return modelo;
 		}
