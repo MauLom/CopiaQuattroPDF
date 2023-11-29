7bc7873d7507e9c759fb8025047102653344c5a6
Adan Albanil Cruz <aalbanil@aiou-ubu10-adana>
2023-11-28 18:51:11 -0600

    Actualizar SegurosMtySalud2.java

--- src/main/java/com/copsis/models/segurosMty/SegurosMtySalud2.java
+++ src/main/java/com/copsis/models/segurosMty/SegurosMtySalud2.java
@@ -1,6 +1,7 @@
 package com.copsis.models.segurosMty;
 
 
+import com.copsis.constants.ConstantsValue;
 import com.copsis.models.DataToolsModel;
 import com.copsis.models.EstructuraJsonModel;
 
@@ -15,7 +16,13 @@ public class SegurosMtySalud2 {
 	 int fin = 0;
      contenido = fn.remplazarMultiple(contenido, fn.remplazosGenerales());
          try {
-            
+            	modelo.setCia(39);			
+			modelo.setTipo(3);
+          
+            inicio = contenido.indexOf(ConstantsValue.PLAN);
+			fin = contenido.indexOf("COBERTURAS CONTRATADAS");
+           
+			newcontenido.append(fn.extracted(inicio, fin, contenido));
             return modelo;
          } catch (Exception ex) {
             modelo.setError(
