e9f619838b136d0164040f2470f2b1ccc12346c6
Adan Albanil <aalbanil@AIOU-UBU10-AdnA>
2024-02-27 13:58:30 -0600

    Actualizar ConstantsValue.java, DataToolsModel.java y 3 más archivos...

---
+++ src/main/java/com/copsis/models/sspins/SspInsModel.java
@@ -0,0 +1,25 @@
+package com.copsis.models.sspins;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+import com.copsis.models.DataToolsModel;
+import com.copsis.models.EstructuraJsonModel;
+
+public class SspInsModel {
+
+    private DataToolsModel fn = new DataToolsModel();
+    private EstructuraJsonModel modelo = new EstructuraJsonModel();
+
+    public EstructuraJsonModel procesar(PDFTextStripper stripper, PDDocument doc, String contenido) {
+        try {
+            modelo = new SspInsDiversosModel().procesar(fn.caratula(1, 4, stripper, doc));
+            return modelo;
+        } catch (Exception ex) {
+            modelo.setError(
+                    SspInsModel.this.getClass().getTypeName() + " | " + ex.getMessage() + " | " + ex.getCause());
+            return modelo;
+        }
+
+    }
+}
