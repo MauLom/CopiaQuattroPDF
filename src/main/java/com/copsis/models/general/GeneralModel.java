3e57895b34f74461b27c73a8c6c47536f51fc9da
Adan Albanil <aalbanil@AIOU-UBU10-AdnA>
2024-03-22 15:44:14 -0600

    Actualizar GeneralModel.java y GneralDiversosBModel.java

--- src/main/java/com/copsis/models/general/GeneralModel.java
+++ src/main/java/com/copsis/models/general/GeneralModel.java
@@ -1,58 +1,68 @@
 package com.copsis.models.general;
 
+import java.io.IOException;
+
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
 
 import com.copsis.models.DataToolsModel;
 import com.copsis.models.EstructuraJsonModel;
 
-
 public class GeneralModel {
-	
+
 	private DataToolsModel fn = new DataToolsModel();
 	private EstructuraJsonModel modelo = new EstructuraJsonModel();
 	private String contenido;
-	
+
 	public EstructuraJsonModel procesar(PDFTextStripper pdfStripper, PDDocument pdDoc, String contenido) {
-		try {	
-
-			int tipo = fn.tipoPoliza(contenido);
-			if(contenido.contains("PÓLIZA INDIVIDUAL / FAMILIAR")) {
-				tipo =2;
-			}
-			if(contenido.contains("MARÍTIMO Y TRANSPORTES-CARGA ESPECÍFICA")) {
-				tipo =4;
-			}
-
-			if(contenido.contains("PAQUETE EMPRESARIAL")) {
-				tipo =4;
-			}
-			if(fn.caratula(2, 2, pdfStripper, pdDoc).contains("PÓLIZA DE AUTOMÓVILES")){
-				tipo =1;
-
-			}
-
-			 switch (tipo) {
-			 case 1:
-				 modelo = new GeneralAutosModel().procesar(fn.caratula(1, 3, pdfStripper, pdDoc));	
-				 break;
-			 case 2:
-				 modelo = new GeneralSaludModel().procesar(fn.caratula(1, 3, pdfStripper, pdDoc));	
-				 break;
-		     case 4:
-				if(contenido.contains("PAQUETE EMPRESARIAL")) {
-					modelo = new GeneralDiversosAModel().procesar(fn.caratula(1, 3, pdfStripper, pdDoc));	
-				}else{
-					modelo = new GeneralDiversosModel().procesar(fn.caratula(1, 3, pdfStripper, pdDoc));	
-				}
+		try {
 			
-				 break;	 
-			 }
-					
+			int tipo = fn.tipoPoliza(contenido);
+			if (contenido.contains("PÓLIZA INDIVIDUAL / FAMILIAR")) {
+				tipo = 2;
+			}
+			if (contenido.contains("MARÍTIMO Y TRANSPORTES-CARGA ESPECÍFICA")) {
+				tipo = 4;
+			}
+
+			if (contenido.contains("PAQUETE EMPRESARIAL")
+					|| fn.caratula(1, 2, pdfStripper, pdDoc).contains("Maquinaria Segura")) {
+				tipo = 4;
+			}
+			if (fn.caratula(2, 2, pdfStripper, pdDoc).contains("PÓLIZA DE AUTOMÓVILES")) {
+				tipo = 1;
+
+			}
+
+			switch (tipo) {
+				case 1:
+					modelo = new GeneralAutosModel().procesar(fn.caratula(1, 3, pdfStripper, pdDoc));
+					break;
+				case 2:
+					modelo = new GeneralSaludModel().procesar(fn.caratula(1, 3, pdfStripper, pdDoc));
+					break;
+				case 4:
+					getIndentifica(pdfStripper, pdDoc, contenido);
+
+					break;
+				default:	
+			}
+
 			return modelo;
 		} catch (Exception ex) {
-			modelo.setError(GeneralModel.this.getClass().getTypeName() + " | " + ex.getMessage() + " | " + ex.getCause());
+			modelo.setError(
+					GeneralModel.this.getClass().getTypeName() + " | " + ex.getMessage() + " | " + ex.getCause());
 			return modelo;
+		}
+	}
+
+	private void getIndentifica(PDFTextStripper pdfStripper, PDDocument pdDoc, String contenido) throws IOException {
+		if (contenido.contains("PAQUETE EMPRESARIAL")) {
+			modelo = new GeneralDiversosAModel().procesar(fn.caratula(1, 3, pdfStripper, pdDoc));
+		} else if (fn.caratula(1, 2, pdfStripper, pdDoc).contains("Maquinaria Segura")) {
+			modelo = new GneralDiversosBModel().procesar(fn.caratula(1, 3, pdfStripper, pdDoc));
+		} else {
+			modelo = new GeneralDiversosModel().procesar(fn.caratula(1, 3, pdfStripper, pdDoc));
 		}
 	}
 }
